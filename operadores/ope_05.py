334cf3b43c3c37f55e83c222ee614150d5eb7a2c
luizzz <115375085+LuizRTz@users.noreply.github.com>
2024-11-22 12:07:44 -0300

    Update ope_05.py

---
+++ operadores/ope_05.py
@@ -0,0 +1,8 @@
+# Solicita a temperatura em Celsius ao usuário
+celsius = float(input("Digite a temperatura em Celsius: "))
+
+# Converte a temperatura de Celsius para Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Exibe o resultado
+print(f"A temperatura de {celsius}°C é igual a {fahrenheit}°F.")
