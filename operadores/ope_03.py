57d443a763b874d1a47e74aa5cc59e46f8b13026
luizzz <115375085+LuizRTz@users.noreply.github.com>
2024-11-22 12:07:13 -0300

    Update ope_03.py

---
+++ operadores/ope_03.py
@@ -0,0 +1,8 @@
+# Solicita o lado do quadrado ao usuário
+lado = float(input("Digite o valor do lado do quadrado: "))
+
+# Calcula a área do quadrado
+area = lado ** 2
+
+# Exibe o resultado
+print(f"A área do quadrado com lado {lado} é: {area}")
