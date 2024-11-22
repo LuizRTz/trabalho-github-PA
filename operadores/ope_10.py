5321c5f7eff4a07d52f655564a3c0924f2b3804a
luizzz <115375085+LuizRTz@users.noreply.github.com>
2024-11-22 12:08:59 -0300

    Update ope_10.py

---
+++ operadores/ope_10.py
@@ -0,0 +1,8 @@
+# Solicita a idade do usuário
+idade_em_anos = int(input("Informe sua idade em anos: "))
+
+# Converte a idade em anos para dias (considerando 365 dias por ano)
+idade_em_dias = idade_em_anos * 365
+
+# Exibe o resultado
+print(f"Sua idade em dias é aproximadamente: {idade_em_dias} dias.")
